747561ed9f8cd7597d3fa5d65483e028cd23aa35
iiFouzi <60854634+iiFouzi@users.noreply.github.com>
2020-08-16 13:34:07 +0100

    Update Main.php

--- src/blockpos/iiFouzi/Main.php
+++ src/blockpos/iiFouzi/Main.php
@@ -25,9 +25,9 @@ class Main extends PluginBase implements Listener
     $player = $event->getPlayer();
     $block = $event->getBlock;
     if($player->getAction() === PlayerIneteractEvent::RIGHT_CLICK_BLOCK){
-      $x = $block->getPos(X);
-      $y = $block->getPos(Y);
-      $z = $block->getPos(Z);
+      $x = $block->getX();
+      $y = $block->getY();
+      $z = $block->getZ();
       $player->sendMessage(TF::GREEN . "Tapped Block coordinates are\n Z: " . $z . "\n Y: " . $y . "\n X: " . $x)
     }
   }
